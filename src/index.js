215eac9e3a09a246de60d23cd221bfab37ab180d
Nik Rowell <nik@nikrowell.com>
2018-08-18 20:36:18 -0600

    getting transitions mostly working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,187 @@
 import { Component, render, cloneElement, h } from 'preact';
 import { Route, Link } from './router';
+import { isFunction } from './utils';
+import Canvas from './canvas';
 import '../scss/index.scss';
 
 const debug = (value) => (
   <pre>{JSON.stringify(value, null, 2)}</pre>
 );
+
+class Transition extends Component {
+
+  constructor(props) {
+
+    super(props);
+
+    const children = this.getChildMapping(this.props.children);
+    this.state = {children};
+
+    this.refs = {};
+    this.keysToEnter = [];
+    this.keysToLeave = [];
+    this.entering = {};
+    this.leaving = {};
+
+    this.enter = this.enter.bind(this);
+    this.leave = this.leave.bind(this);
+  }
+
+  componentDidMount() {
+    const children = this.state.children;
+    for (let key in children) this.enter(key);
+  }
+
+  componentWillReceiveProps(nextProps) {
+
+    const prevChildren = this.state.children;
+    const nextChildren = this.getChildMapping(nextProps.children);
+
+    this.setState({
+      children: Object.assign({}, prevChildren, nextChildren)
+    });
+
+    for (let key in nextChildren) {
+      const hasPrev = prevChildren.hasOwnProperty( key);
+      if (nextChildren[key] && (!hasPrev || this.leaving[key])) {
+        this.keysToEnter.push(key);
+      }
+    }
+
+    for (let key in prevChildren) {
+      const hasNext = nextChildren.hasOwnProperty(key);
+      if (prevChildren[key] && !hasNext) {
+        this.keysToLeave.push(key);
+      }
+    }
+  }
+
+  componentDidUpdate() {
+
+    const keysToEnter = this.keysToEnter;
+    const keysToLeave = this.keysToLeave;
+
+    switch (this.props.mode) {
+
+      case 'out-in':
+
+        this.keysToLeave = [];
+
+        if (keysToLeave.length) {
+          keysToLeave.forEach(this.leave);
+        } else {
+          this.keysToEnter = [];
+          keysToEnter.forEach(this.animateIn);
+        }
+        break;
+
+      case 'in-out':
+
+        this.keysToEnter = [];
+        this.keysToLeave = [];
+
+        if (keysToEnter.length) {
+          Promise.all(keysToEnter.map(this.enter)).then(() => keysToLeave.forEach(this.leave));
+        } else {
+          keysToLeave.forEach(this.leave);
+        }
+
+        break;
+
+      default:
+        this.keysToEnter = [];
+        this.keysToLeave = [];
+        keysToEnter.forEach(this.enter);
+        keysToLeave.forEach(this.leave);
+        break;
+    }
+  }
+
+  getChildMapping(children) {
+    return children.reduce((result, child) => {
+      const key = child.key;
+      result[key] = child;
+      return result;
+    }, {});
+  }
+
+  enter(key) {
+
+    const component = this.refs[key];
+    const complete = this.enterComplete.bind(this, key);
+
+    const promise = new Promise(resolve => {
+      isFunction(component.animateIn) ? component.animateIn(resolve) : resolve();
+    }).then(complete);
+
+    this.entering[key] = promise;
+    return promise;
+  }
+
+  enterComplete(key) {
+    delete this.entering[key];
+    const currentChildren = this.state.children;
+    if (!currentChildren.hasOwnProperty(key)) {
+      this.leave(key);
+    }
+  }
+
+  leave(key) {
+
+    const component = this.refs[key];
+    const complete = this.leaveComplete.bind(this, key);
+
+    const promise = new Promise(resolve => {
+      isFunction(component.animateOut) ? component.animateOut(resolve) : resolve();
+    }).then(complete);
+
+    this.leaving[key] = promise;
+    return promise;
+  }
+
+  leaveComplete(key) {
+    const children = this.state.children;
+    delete children[key];
+    delete this.leaving[key];
+    this.setState({children});
+  }
+
+  render() {
+
+    const children = this.state.children;
+
+    return (
+      <div>
+
+        {Object.keys(children).map(key => {
+          return cloneElement(children[key], {ref: el => this.refs[key] = el});
+        })}
+
+        {debug(this.props.mode)}
+        {debug(this.state)}
+      </div>
+    );
+  }
+}
+
+class Tester extends Component {
+
+  animateIn(done) {
+    const el = this.base;
+    el.style.opacity = 0;
+    el.style.transition = 'opacity 1s ease';
+    el.addEventListener('transitionend', done);
+    setTimeout(() => el.style.opacity = 1, 500);
+  }
+
+  animateOut(done) {
+    setTimeout(done, 1000);
+  }
+
+  render() {
+    return <div>I'm a component with animateIn <strong>{this.props.children}</strong></div>
+  }
+}
 
 const About = (props) => (
   <div>
@@ -31,122 +208,6 @@ class Project extends Component {
     );
   }
 }
-
-
-
-import { isFunction } from './utils';
-
-
-class Transition extends Component {
-
-  constructor(props) {
-    super(props);
-    const mapping = this.getChildMapping(this.props.children);
-    this.state = {mapping};
-    this.entering = {};
-    // this.leaving = {};
-    this.refs = {};
-  }
-
-  componentDidMount() {
-    const mapping = this.state.mapping;
-    for (let key in mapping) this.animateIn(key);
-  }
-
-  componentWillReceiveProps(nextProps) {
-
-    const prevChildren = this.state.children;
-    const nextChildren = this.getChildMapping(nextProps.children);
-    const mapping = Object.assign({}, prevChildren, nextChildren);
-
-    this.setState({mapping});
-
-    for (let key in prevChildren) {
-      // this.outgoing.push(key);
-    }
-
-    for (let key in nextChildren) {
-      // this.incoming.push(key);
-    }
-  }
-
-  componentDidUpdate() {
-
-    // const incoming = this.incoming;
-    // const outgoing = this.outgoing;
-
-    switch (this.props.mode) {
-      case 'in-out':
-        break;
-      case 'out-in':
-        break;
-      default:
-        // this.incoming = [];
-        // this.outgoing = [];
-        // incoming.forEach(this.animateIn);
-        // outgoing.forEach(this.animateOut);
-        break;
-    }
-  }
-
-  getChildMapping(children) {
-    return children.reduce((result, child) => {
-      const key = child.key;
-      result[key] = child;
-      return result;
-    }, {});
-  }
-
-  animateIn(key) {
-    const component = this.refs[key];
-    if (isFunction(component.animateIn)) {
-      this.entering[key] = true;
-      component.animateIn(() => delete this.entering[key]);
-    }
-  }
-
-  animateOut(key) {
-    console.log('animateOut', key);
-  }
-
-  render() {
-
-    const mapping = this.state.mapping;
-    const children = Object.keys(mapping).map(key => {
-      return cloneElement(mapping[key], {ref: el => this.refs[key] = el});
-    });
-
-    return (
-      <div>
-        {children}
-        {debug(this.props.mode)}
-        {debug(this.state)}
-      </div>
-    );
-  }
-}
-
-
-
-class Tester extends Component {
-
-  animateIn(done) {
-    const el = this.base;
-    el.style.opacity = 0;
-    el.style.transition = 'opacity 1s ease';
-    el.addEventListener('transitionend', done);
-    setTimeout(() => el.style.opacity = 1, 500);
-  }
-
-  animateOut(done) {
-    // setTimeout(done, 1000);
-  }
-
-  render() {
-    return <div>I'm a component with animateIn <strong>{this.props.children}</strong></div>
-  }
-}
-
 
 class App extends Component {
 
@@ -180,17 +241,27 @@ class App extends Component {
           <hr />
           <Route path="/projects/:id" view={Project} />
         </div>
-        <button onClick={e => this.setState({items: items.concat(Math.random())})}>Click Me</button>
+
+        <button onClick={e => {
+          this.setState({items: items.concat(Math.random())})
+        }}>Add One</button>
+
+        <button onClick={e => {
+          const index = Math.floor(Math.random() * items.length);
+          this.setState({items: [...items.slice(0, index), ...items.slice(index + 1)]})
+        }}>Remove One</button>
+
         <select value={this.state.mode || 'simultaneous'} onChange={e => this.setState({mode: event.target.value})}>
           <option value="simultaneous">simultaneous</option>
           <option value="in-out">in-out</option>
           <option value="out-in">out-in</option>
         </select>
         <Transition mode={this.state.mode}>
-          <div key="func">func</div>
           <Tester key="comp" />
           {items.map(n => n > 0.5 ? <Tester key={n}>{n}</Tester> : <div key={`key-${n}`}>{n}</div>)}
         </Transition>
+        {/* <Canvas /> */}
+        {debug(items)}
       </div>
     );
   }
